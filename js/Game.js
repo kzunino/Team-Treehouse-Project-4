5e01d6549531102bf3f1210a0a469c0abac81cc4
Kyle <kzunino@gmail.com>
2019-05-10 20:44:22 -0700

    removeLife now changes

--- js/Game.js
+++ js/Game.js
@@ -35,10 +35,22 @@ getRandomPhrase() {
 * Begins game by selecting a random phrase and displaying it to user
 */
 startGame(){
+  this.missed = 0;
+  this.phrases = this.createPhrases();
+  this.activePhrase = this.getRandomPhrase().toLowerCase();
   const overlay = $('#overlay');
-  overlay.hide();
+  const clearPhrase = $('ul');
+  const scoreboard = $('ol');
   const randomPhrase = game.getRandomPhrase();
-  const phrase = new Phrase(randomPhrase.phrase);
+  const phrase = new Phrase(this.activePhrase.phrase);
+  clearPhrase.empty();
+  scoreboard.empty();
+  scoreboard.append('<li class="tries"><img src="images/liveHeart.png" alt="Heart Icon" height="35" width="30"></li>')
+  scoreboard.append('<li class="tries"><img src="images/liveHeart.png" alt="Heart Icon" height="35" width="30"></li>')
+  scoreboard.append('<li class="tries"><img src="images/liveHeart.png" alt="Heart Icon" height="35" width="30"></li>')
+  scoreboard.append('<li class="tries"><img src="images/liveHeart.png" alt="Heart Icon" height="35" width="30"></li>')
+  scoreboard.append('<li class="tries"><img src="images/liveHeart.png" alt="Heart Icon" height="35" width="30"></li>')
+  overlay.hide();
   phrase.addPhraseToDisplay();
 }
 
@@ -47,34 +59,53 @@ handleInteractions() {
 
 }
 
+/**
+* Checks for winning move
+* @return {boolean} True if game has been won, false if game wasn't
+won
+*/
 
 checkForWin(){
   const lettersHidden = $('.hide');
+  const win = 'win';
   if (lettersHidden.length === 0) {
     console.log('winner');
-    game.gameOver();
+    game.gameOver(true);
   }
 }
 
+/**
+* Increases the value of the missed property
+* Removes a life from the scoreboard
+* Checks if player has remaining lives and ends game if player is out
+*/
+
 removeLife(){
-  const life = $('ol li');
+  const life = $('ol .tries');
+  const ol = $('ol')
   life.first().remove()
+  ol.prepend('<li><img src="images/lostHeart.png" alt="Heart Icon" height="35" width="30"></li>');
   if (life.length === 1){
     game.gameOver();
   }
 }
 
-gameOver(){
+/**
+* Displays game over message
+* @param {boolean} gameWon - Whether or not the user won the game
+*/
+
+gameOver(outcome){
   const overlay = $('#overlay');
-  const overlayWin = $('#game-over-message').html('You won!');
-  const overlayLose = $('#game-over-message').html('You lost!');
-  if (){
+  if (outcome === true){
+    const overlayWin = $('#game-over-message').html('You won!');
     overlay.show()
   }else{
+    const overlayLose = $('#game-over-message').html('You lost!');
     overlay.show()
   }
 
-}
+ }
 
 
 }
